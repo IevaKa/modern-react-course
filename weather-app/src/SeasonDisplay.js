09ce8feaa865b6391676927ae9a9ae99516df163
IevaKa <kaziukonyte.ieva@gmail.com>
2020-08-23 00:29:32 +0300

    add some weather app styling

--- weather-app/src/SeasonDisplay.js
+++ weather-app/src/SeasonDisplay.js
@@ -1,7 +1,35 @@
+import './SeasonDisplay.css'
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>This is the season display!</div>;
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    icon: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    icon: 'snowflake'
+  }
+}
+
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat < 0 ? 'winter' : 'summer'
+  }
+}
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, icon } = seasonConfig[season];
+    return (
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${icon} icon`}></i>
+        <h1>{ text }</h1>
+        <i className={`icon-right massive ${icon} icon`}></i>
+      </div>
+    )
 };
 
 export default SeasonDisplay;
