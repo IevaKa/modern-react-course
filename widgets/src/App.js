cf3b4cfbba66e4177413d4eca00f5b71343db439
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2020-09-05 21:43:26 +0200

    practice hooks

---
+++ widgets/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a frontend javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content:
+      "ReactJS is just simpler to grasp right away. The component-based approach, well-defined lifecycle, and use of just plain JavaScript make React very simple to learn, build a professional web (and mobile applications), and support it. React uses a special syntax called JSX which allows you to mix HTML with JavaScript.",
+  },
+  {
+    title: "How do you use React?",
+    content:
+      "In React, instead of using regular JavaScript for templating, it uses JSX. JSX is a simple JavaScript that allows HTML quoting and uses these HTML tag syntax to render subcomponents. HTML syntax is processed into JavaScript calls of React Framework. We can also write in pure old JavaScript.",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
+
+export default App;
