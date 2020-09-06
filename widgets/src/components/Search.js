c150ff648412d4c9840160c2040af6f81864e4f6
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2020-09-06 14:38:01 +0200

    understand useEffect, setTimeout for search

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("javascript");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   // Second argument:
@@ -10,20 +11,51 @@ const Search = () => {
   // 2. (Nothing) ------> Run at initial render; and after every rerender
   // 3. [term] -----> Run at initial render; run after every rerender if the term has changed
   // since the last render
+
+  // this useEffect watches the changes of "term".
+  // if the "term" hasn't changed for 1 second, debounced term gets updated
   useEffect(() => {
-    (async () => {
+    const timerId = setTimeout(() => setDebouncedTerm(term), 1000);
+    return () => clearTimeout(timerId);
+  }, [term]);
+
+  // this useEffect watched the changes of "debouncedTerm".
+  // Once debounced term changes (first useEffect function) and on initial renderof the component,
+  // I call the API, and render the results
+  useEffect(() => {
+    const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
           format: "json",
           origin: "*",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
-    })();
-  }, [term]);
+    };
+    search();
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -37,6 +69,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
